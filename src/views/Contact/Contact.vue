ac60032794735670e1ed27de6a1394c9fe78eb35
masterlineAlejandro <ajimenez@masterline-logistics.com.co>
2023-01-05 16:20:39 -0500

    formuylario de ver contacto

--- src/views/Contact/Contact.vue
+++ src/views/Contact/Contact.vue
@@ -1,209 +1,212 @@
 <template>
 
-    <div>  
-      <!-- Table Container Card -->
-      <b-card no-body class="mb-0">
-  
-        <div class="m-2">
-  
-          <!-- Table Top -->
-          <b-row>
-            <!-- Per Page -->
-            <b-col cols="12" md="6" class="d-flex align-items-center justify-content-start mb-1 mb-md-0">
-              <label>Mostrar</label>
-              <v-select v-model="tableSettings.perPage" :options="perPageOptions" :clearable="false"
-                class="per-page-selector d-inline-block mx-50" />
-              <label>registros</label>
-            </b-col>
-            <!-- Search -->
-            <b-col cols="12" md="6">
-              <div class="d-flex align-items-center justify-content-end">
-                <b-button variant="primary" @click="newContact()">
-                  <span class="text-nowrap">Nuevo contacto</span>
-                </b-button>
-              </div>
-            </b-col>
-          </b-row>
-        </div>
-        <b-table ref="refContactListTable" class="position-relative" :items="contact" responsive :fields="tableColumns"
-          primary-key="id" :sort-by.sync="tableSettings.sortBy" show-empty empty-text="No se encontraron datos"
-          :sort-desc.sync="tableSettings.sortDesc">
-      
-          <!-- Column: Actions -->
-          <template #cell(actions)="data">
-            <b-button
-            variant="primary" class="btn-icon rounded-circle ml-2" v-b-tooltip.hover.v-primary title="Ver">
-              <feather-icon icon="EyeIcon" />
-            </b-button>
-            <b-button
-              class="btn-icon rounded-circle ml-2" v-b-tooltip.hover.v-primary title="Editar">
-              <feather-icon icon="EditIcon" />
-            </b-button>
-            <b-button variant="danger" class="btn-icon rounded-circle ml-2"
-              v-b-tooltip.hover.v-primary title="Eliminar">
-              <feather-icon icon="Trash2Icon" />
-            </b-button>
-          </template>
-        </b-table>
-        <div class="mx-2 mb-2">
-          <b-row>
-  
-            <b-col cols="12" sm="6" class="d-flex align-items-center justify-content-center justify-content-sm-start">
-              <span class="text-muted">Viendo del {{ dataMeta.from }} al {{ dataMeta.to }} de {{ dataMeta.of }}
-                registros</span>
-            </b-col>
-            <!-- Pagination -->
-            <b-col cols="12" sm="6" class="d-flex align-items-center justify-content-center justify-content-sm-end">
-  
-              <b-pagination v-model="tableSettings.page" :total-rows="totalRows" :per-page="tableSettings.perPage"
-                first-number last-number class="mb-0 mt-1 mt-sm-0" prev-class="prev-item" next-class="next-item">
-                <template #prev-text>
-                  <feather-icon icon="ChevronLeftIcon" size="18" />
-                </template>
-                <template #next-text>
-                  <feather-icon icon="ChevronRightIcon" size="18" />
-                </template>
-              </b-pagination>
-            </b-col>
-          </b-row>
-        </div>
-      </b-card>    
-    </div>
-  </template>
-  
-  <script>
-  extend('required', {
-    ...required,
-    message: 'El campo {_field_} es obligatorio'
-  });
-  import { extend } from 'vee-validate'
-  import { ValidationProvider, ValidationObserver, localize } from 'vee-validate'
-  import { required } from '@validations'
-  import {
-    BCard, BRow, BCol, BFormInput, BButton, BTable, BMedia, BAvatar, BLink,
-    BBadge, BDropdown, BDropdownItem, BPagination, BFormGroup, BForm, VBTooltip
-  } from 'bootstrap-vue'
-  import vSelect from 'vue-select'
-  import Ripple from 'vue-ripple-directive'
-  export default {
-    name: 'Contacts',
-    components: {
-      BCard,
-      BRow,
-      BCol,
-      BFormInput,
-      BButton,
-      BTable,
-      BMedia,
-      BAvatar,
-      BLink,
-      BBadge,
-      BDropdown,
-      BDropdownItem,
-      BPagination,
-      vSelect,
-      BFormGroup,
-      BForm,
-      ValidationProvider,
-      ValidationObserver,
-      VBTooltip
-    },
-    data() {
-      return {
-        update: false,
-        modal_password: false,
-        formDisabled: false,
-        refContactListTable: null,
-        perPageOptions: [10, 25, 50, 100],
+  <div>
+    <!-- Table Container Card -->
+    <b-card no-body class="mb-0">
+
+      <div class="m-2">
+
+        <!-- Table Top -->
+        <b-row>
+          <!-- Per Page -->
+          <b-col cols="12" md="6" class="d-flex align-items-center justify-content-start mb-1 mb-md-0">
+            <label>Mostrar</label>
+            <v-select v-model="tableSettings.perPage" :options="perPageOptions" :clearable="false"
+              class="per-page-selector d-inline-block mx-50" />
+            <label>registros</label>
+          </b-col>
+          <!-- Search -->
+          <b-col cols="12" md="6">
+            <div class="d-flex align-items-center justify-content-end">
+              <b-button variant="primary" @click="newContact()">
+                <span class="text-nowrap">Nuevo contacto</span>
+              </b-button>
+            </div>
+          </b-col>
+        </b-row>
+      </div>
+      <b-table ref="refContactListTable" class="position-relative" :items="contact" responsive :fields="tableColumns"
+        primary-key="id" :sort-by.sync="tableSettings.sortBy" show-empty empty-text="No se encontraron datos"
+        :sort-desc.sync="tableSettings.sortDesc">
+
+        <!-- Column: Actions -->
+        <template #cell(actions)="data">
+          <b-button @click="showContact(data.item.id)" variant="primary" class="btn-icon rounded-circle ml-2" v-b-tooltip.hover.v-primary title="Ver">
+            <feather-icon icon="EyeIcon" />
+          </b-button>
+          <b-button class="btn-icon rounded-circle ml-2" v-b-tooltip.hover.v-primary title="Editar">
+            <feather-icon icon="EditIcon" />
+          </b-button>
+          <b-button variant="danger" class="btn-icon rounded-circle ml-2" v-b-tooltip.hover.v-primary title="Eliminar">
+            <feather-icon icon="Trash2Icon" />
+          </b-button>
+        </template>
+      </b-table>
+      <div class="mx-2 mb-2">
+        <b-row>
+
+          <b-col cols="12" sm="6" class="d-flex align-items-center justify-content-center justify-content-sm-start">
+            <span class="text-muted">Viendo del {{ dataMeta.from }} al {{ dataMeta.to }} de {{ dataMeta.of }}
+              registros</span>
+          </b-col>
+          <!-- Pagination -->
+          <b-col cols="12" sm="6" class="d-flex align-items-center justify-content-center justify-content-sm-end">
+
+            <b-pagination v-model="tableSettings.page" :total-rows="totalRows" :per-page="tableSettings.perPage"
+              first-number last-number class="mb-0 mt-1 mt-sm-0" prev-class="prev-item" next-class="next-item">
+              <template #prev-text>
+                <feather-icon icon="ChevronLeftIcon" size="18" />
+              </template>
+              <template #next-text>
+                <feather-icon icon="ChevronRightIcon" size="18" />
+              </template>
+            </b-pagination>
+          </b-col>
+        </b-row>
+      </div>
+    </b-card>
+  </div>
+</template>
+
+<script>
+extend('required', {
+  ...required,
+  message: 'El campo {_field_} es obligatorio'
+});
+import { extend } from 'vee-validate'
+import { ValidationProvider, ValidationObserver, localize } from 'vee-validate'
+import { required } from '@validations'
+import {
+  BCard, BRow, BCol, BFormInput, BButton, BTable, BMedia, BAvatar, BLink,
+  BBadge, BDropdown, BDropdownItem, BPagination, BFormGroup, BForm, VBTooltip
+} from 'bootstrap-vue'
+import vSelect from 'vue-select'
+import Ripple from 'vue-ripple-directive'
+export default {
+  name: 'Contacts',
+  components: {
+    BCard,
+    BRow,
+    BCol,
+    BFormInput,
+    BButton,
+    BTable,
+    BMedia,
+    BAvatar,
+    BLink,
+    BBadge,
+    BDropdown,
+    BDropdownItem,
+    BPagination,
+    vSelect,
+    BFormGroup,
+    BForm,
+    ValidationProvider,
+    ValidationObserver,
+    VBTooltip
+  },
+  data() {
+    return {
+      update: false,
+      modal_password: false,
+      formDisabled: false,
+      refContactListTable: null,
+      perPageOptions: [10, 25, 50, 100],
+      searchQuery: '',
+      tableColumns: [
+        { key: 'id', label: 'Id', sortable: true },
+        { key: 'fullname', label: 'Nombre', sortable: true },
+        { key: 'number_identification', label: 'Identificación' },
+        { key: 'phone', label: 'Télefono' },
+        { key: 'actions', label: 'Acciones' },
+      ],
+      sortBy: 'id',
+      isSortDirDesc: true,
+      totalRows: 0,
+      dataMeta: {
+        from: 0,
+        to: 0,
+        of: 0,
+      },
+      dataTable: [],
+      showLoadingTable: false,
+      tableSettings: {
         searchQuery: '',
-        tableColumns: [
-          { key: 'id', label: 'Id', sortable: true },
-          { key: 'fullname', label: 'Nombre', sortable: true },
-          { key: 'number_identification', label: 'Identificación' },
-          { key: 'phone', label: 'Télefono' },
-          { key: 'actions', label: 'Acciones' },
-        ],
+        perPage: 10,
+        page: 1,
         sortBy: 'id',
-        isSortDirDesc: true,
-        totalRows: 0,
-        dataMeta: {
-          from: 0,
-          to: 0,
-          of: 0,
-        },
-        dataTable: [],
-        showLoadingTable: false,
-        tableSettings: {
-          searchQuery: '',
-          perPage: 10,
-          page: 1,
-          sortBy: 'id',
-          sortDesc: true,
-        },
-        formDataEdit: {},
-        contact: [],
-      }
-    },
-    directives: {
-      Ripple,
-      'b-tooltip': VBTooltip,
-    },
-    watch: {
-      "tableSettings.sortBy": {
-        handler(val) {
-          this.fetchContacts()
-        },
+        sortDesc: true,
       },
-      "tableSettings.sortDesc": {
-        handler(val) {
-          this.fetchContacts()
-        },
-      },
-      "tableSettings.perPage": {
-        handler(val) {
-          this.fetchContacts()
-        },
-      },
-      "tableSettings.searchQuery": {
-        handler(val) {
-          this.fetchContacts()
-        },
-      },
-      "tableSettings.page": {
-        handler(val) {
-          this.fetchContacts()
-        },
+      formDataEdit: {},
+      contact: [],
+    }
+  },
+  directives: {
+    Ripple,
+    'b-tooltip': VBTooltip,
+  },
+  watch: {
+    "tableSettings.sortBy": {
+      handler(val) {
+        this.fetchContacts()
       },
     },
-    created() {
-      this.fetchContacts();
-    },
-    methods: {
-      fetchContacts() {
-        this.$http.get('contact/list').then((response) => {
-          this.contact = response.data.contacts
-        })
+    "tableSettings.sortDesc": {
+      handler(val) {
+        this.fetchContacts()
       },
-      dataMetaCounter() {
-        const localItemsCount = this.dataTable.length
-        this.dataMeta.from = this.tableSettings.perPage * (this.tableSettings.page - 1) + (localItemsCount ? 1 : 0)
-        this.dataMeta.to = this.tableSettings.perPage * (this.tableSettings.page - 1) + localItemsCount
-        this.dataMeta.of = this.totalRows
-      },
-      newContact() {
-        this.$router.push('/contacts/new-contact');
-      }
     },
-  }
-  </script>
-  
-  <style lang="scss" scoped>
-  .per-page-selector {
-    width: 90px;
-  }
-  </style>
-  
-  <style lang="scss">
-  @import '@core/scss/vue/libs/vue-select.scss';
-  @import '@core/scss/vue/libs/vue-sweetalert.scss';
-  </style>
+    "tableSettings.perPage": {
+      handler(val) {
+        this.fetchContacts()
+      },
+    },
+    "tableSettings.searchQuery": {
+      handler(val) {
+        this.fetchContacts()
+      },
+    },
+    "tableSettings.page": {
+      handler(val) {
+        this.fetchContacts()
+      },
+    },
+  },
+  created() {
+    this.fetchContacts();
+  },
+  methods: {
+    fetchContacts() {
+      this.$http.get('contact/list').then((response) => {
+        this.contact = response.data.contacts
+      })
+    },
+    dataMetaCounter() {
+      const localItemsCount = this.dataTable.length
+      this.dataMeta.from = this.tableSettings.perPage * (this.tableSettings.page - 1) + (localItemsCount ? 1 : 0)
+      this.dataMeta.to = this.tableSettings.perPage * (this.tableSettings.page - 1) + localItemsCount
+      this.dataMeta.of = this.totalRows
+    },
+    newContact() {
+      this.$router.push('/contacts/new-contact');
+    },
+    showContact(id) {
+      this.$router.push({
+        name: 'contacts/new-contact',
+        params: { id: id }
+      });
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.per-page-selector {
+  width: 90px;
+}
+</style>
+
+<style lang="scss">
+@import '@core/scss/vue/libs/vue-select.scss';
+@import '@core/scss/vue/libs/vue-sweetalert.scss';
+</style>
--- src/views/Contact/StoreContact.vue
+++ src/views/Contact/StoreContact.vue
@@ -18,7 +18,7 @@
                         <b-button v-ripple.400="'rgba(113, 102, 240, 0.15)'" class="mt-1 d-flex"
                             variant="outline-primary" @click="toggleIndeterminate">
                             <p class="mb-0">Cliente</p>
-                            <b-form-checkbox class="ml-5 mt-0" v-model="form.checked1"
+                            <b-form-checkbox class="ml-5 mt-0" v-model="form.checked1" :disabled="disabled"
                                 :indeterminate1.sync="indeterminate1">
 
                             </b-form-checkbox>
@@ -26,7 +26,7 @@
                         <b-button v-ripple.400="'rgba(113, 102, 240, 0.15)'" class="mt-1 d-flex ml-5"
                             variant="outline-primary" @click="toggleIndeterminate2">
                             <p class="mb-0">Proveedor</p>
-                            <b-form-checkbox class="ml-5 mt-0" v-model="form.checked2"
+                            <b-form-checkbox class="ml-5 mt-0" v-model="form.checked2" :disabled="disabled"
                                 :indeterminate2.sync="indeterminate2">
 
                             </b-form-checkbox>
@@ -40,27 +40,29 @@
                     </b-col>
                     <b-col cols="12" md="4" class="d-flex align-items-center justify-content-center">
                         <b-form-input v-model="form.number_identify" v-if="form.type_identification.title == 'RNC'"
-                            placeholder="Numero RNC" />
+                            :disabled="disabled" placeholder="Numero RNC" />
                         <b-form-input v-model="form.number_identify"
-                            v-else-if="form.type_identification.title == 'Cédula'" placeholder="Numero Cédula" />
+                            v-else-if="form.type_identification.title == 'Cédula'" :disabled="disabled"
+                            placeholder="Numero Cédula" />
                         <b-form-input v-model="form.number_identify"
                             v-else-if="form.type_identification.title == 'Pasaporte (Identificador extranjero)'"
-                            placeholder="Numero" />
+                            :disabled="disabled" placeholder="Numero" />
                         <b-form-input v-model="form.number_identify" v-else-if="form.type_identification == ''"
-                            placeholder="RNC o Cédula" />
+                            placeholder="RNC o Cédula" :disabled="disabled" />
                     </b-col>
                     <b-col cols="12" md="4" class="d-flex align-items-center justify-content-center">
-                        <b-form-input v-model="form.full_name" placeholder="Razón social / nombre completo" />
+                        <b-form-input v-model="form.full_name" :disabled="disabled"
+                            placeholder="Razón social / nombre completo" />
                     </b-col>
                 </b-row>
                 <b-row class="mt-2">
                     <b-col cols="12" md="6" class="d-flex align-items-center justify-content-center">
-                        <b-form-input v-model="form.address" placeholder="Dirección" />
+                        <b-form-input v-model="form.address" placeholder="Dirección" :disabled="disabled" />
                     </b-col>
 
                     <b-col cols="12" md="6" class="align-items-center justify-content-center">
                         <v-select :dir="$store.state.appConfig.isRTL ? 'rtl' : 'ltr'" label="title"
-                            :options="typeIdentification" v-model="form.province" placeholder="Municipio / Provincia" />
+                            :options="municipalies" v-model="form.province" placeholder="Municipio / Provincia" />
                     </b-col>
                 </b-row>
                 <b-row class="mt-2">
@@ -96,16 +98,14 @@
                     </b-col>
                 </b-row>
                 <div class="mt-3 mb-2">
-                    <b-form ref="form" :style="{ height: trHeight }" class="repeater-form"
-                        @submit.prevent="repeateAgain">
+                    <b-form ref="form" :style="{ height: trHeight }" class="repeater-form">
 
                         <!-- Row Loop -->
                         <b-row v-for="(item, index) in form.items" :id="item.id" :key="item.id" ref="row">
 
-
                             <b-col md="2">
                                 <b-form-group label="Nombre/Razón social" label-for="Nombre/Razón social">
-                                    <b-form-input v-model="form.bussiness_name" type="text"
+                                    <b-form-input v-model="form.items[index].bussiness_name" type="text"
                                         placeholder="Nombre/Razón social" />
                                 </b-form-group>
                             </b-col>
@@ -113,33 +113,33 @@
 
                             <b-col md="2">
                                 <b-form-group label="Apellido" label-for="Apellido">
-                                    <b-form-input v-model="form.last_name" placeholder="Apellido" />
+                                    <b-form-input v-model="form.items[index].last_name" placeholder="Apellido" />
                                 </b-form-group>
                             </b-col>
 
 
                             <b-col md="2">
                                 <b-form-group label="Correo electrónico" label-for="Correo electrónico">
-                                    <b-form-input v-model="form.email" placeholder="Correo electrónico" />
+                                    <b-form-input v-model="form.items[index].email" placeholder="Correo electrónico" />
                                 </b-form-group>
                             </b-col>
 
 
                             <b-col lg="2" md="2">
                                 <b-form-group label="Teléfono" label-for="Teléfono">
-                                    <b-form-input v-model="form.phone" placeholder="Teléfono" />
+                                    <b-form-input v-model="form.items[index].phone" placeholder="Teléfono" />
                                 </b-form-group>
                             </b-col>
                             <b-col lg="2" md="2">
                                 <b-form-group label="Celular" label-for="Celular">
-                                    <b-form-input v-model="form.mobil" placeholder="Celular" />
+                                    <b-form-input v-model="form.items[index].mobil" placeholder="Celular" />
                                 </b-form-group>
                             </b-col>
 
 
                             <b-col lg="2" md="2" class="mb-50">
                                 <b-button v-ripple.400="'rgba(234, 84, 85, 0.15)'" variant="outline-danger"
-                                    class="mt-0 mt-md-2" @click="removeItem(item)">
+                                    class="mt-0 mt-md-2" @click="removeItem(index)">
                                     <feather-icon icon="XIcon" class="mr-25" />
                                     <span>Eliminar</span>
                                 </b-button>
@@ -269,6 +269,7 @@ export default {
                 phone_one_contact: '',
                 phone_two_contact: ''
             },
+            id: this.$route.params.id,
             indeterminate1: true,
             indeterminate2: true,
             typeIdentification: [
@@ -303,7 +304,8 @@ export default {
             debtsToPay: [
                 { title: 'Cuentas Por Pagar', value: 1 }
             ],
-            selected: { title: 'Square' },
+            disabled: false,
+            municipalies: []
         }
     },
     mounted() {
@@ -317,6 +319,8 @@ export default {
     },
     created() {
         window.addEventListener('resize', this.initTrHeight)
+        this.showContact()
+        this.fetchMunicipalies()
     },
     destroyed() {
         window.removeEventListener('resize', this.initTrHeight)
@@ -334,8 +338,7 @@ export default {
                             },
                             buttonsStyling: false,
                         });
-                        this.fetchUsers();
-                        this.update = false;
+                        this.$router.push('/contacts/all');
                     }
                     if (response.data.code == 500) {
                         this.$swal({
@@ -352,6 +355,40 @@ export default {
                     this.errors = error.response.data.errors;
                 });
         },
+        showContact() {
+            if (this.id) {
+                this.$http.get('/contact/show/' + this.id).then((response) => {
+                    this.disabled = true;
+                    // this.form.type_identification = 'hacer select';
+                    if (response.data.contact.client_of_provider[0].client_of_provider == 'Client') {
+                        this.form.checked1 = true;
+                        this.form.checked2 = false;
+                    }
+                    if (response.data.contact.client_of_provider[0].client_of_provider == 'Provider') {
+                        this.form.checked2 = true;
+                        this.form.checked1 = false;
+                    }
+                    if (response.data.contact.client_of_provider[0].client_of_provider == 'Client' || response.data.contact.client_of_provider[1].client_of_provider == 'Provider') {
+                        this.form.checked2 = true;
+                        this.form.checked1 = true;
+                    }
+                    if (response.data.contact.client_of_provider[0].client_of_provider == 'Provider' || response.data.contact.client_of_provider[1].client_of_provider == 'Client') {
+                        this.form.checked2 = true;
+                        this.form.checked1 = true;
+                    }
+                    this.form.number_identify = response.data.contact.number_identification;
+                    this.form.full_name = response.data.contact.reason;
+                    this.form.address = response.data.contact.address;
+                })
+            } else {
+                this.disabled = false;
+            }
+        },
+        fetchMunicipalies() {
+            this.$http.get('municipaly/list').then((response) => {
+                this.municipalies = response.data.municipalies
+            })
+        },
         toggleIndeterminate() {
             this.indeterminate1 = !this.indeterminate1;
             this.form.checked1 = true;
@@ -362,10 +399,12 @@ export default {
         },
         repeateAgain() {
             this.form.items.push({
-                id: this.nextTodoId = this.nextTodoId,
+                bussiness_name: '',
+                last_name: '',
+                email: '',
+                phone: '',
+                mobil: ''
             })
-            console.log(this.nextTodoId);
-
 
             this.$nextTick(() => {
                 this.trAddHeight(this.$refs.row[0].offsetHeight)
